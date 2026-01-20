f079da150229a80e7e37989a1e1b921a486229b6
temiidaayoo <micekholu@alu.edu.gva.es>
2026-01-20 19:21:55 +0100

    modi

---
+++ src/App.vue
@@ -0,0 +1,18 @@
+<script setup>
+import ecualizer from "./components/ecualizador.vue";
+</script>
+
+<template>
+
+
+    <ecualizer />
+ 
+
+
+
+  <MainLayout />
+</template>
+
+<style scoped>
+
+</style>
---
+++ src/components/pletina.vue
@@ -0,0 +1,19 @@
+<script setup>
+
+</script>
+
+<template>
+
+    <div class="base-pletina">
+        <div>
+            
+        </div>
+        
+    </div>
+
+</template>
+
+<style scoped>
+
+
+</style>
